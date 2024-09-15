7fb40d1e1480a548dc105bb5e326cb4e9ff85490
nika <nika3141535@gmail.com>
2024-09-15 04:18:47 +0400

    add routing

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
+import Layout from "./components/Layout"
+
+
+function App() {
+  
+  const router = createBrowserRouter([
+    {
+      path:'/',
+      element:<Layout/>,
+    }
+  ])
+  
+
+  return (
+    <>
+      <RouterProvider router={router}/>
+    </>
+  )
+}
+
+export default App
